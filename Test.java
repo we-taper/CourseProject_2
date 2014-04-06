9cad1b2149ff09869225de5581be18f2c267da7b
we.taper <cn19998@qq.com>
2014-04-06 13:28:18 +0800

    Modified Test.java to make it more readable.

--- Test.java
+++ Test.java
@@ -1,21 +1,15 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 import static taper.util.PythonCodeStyle.*;
 
-import javax.swing.GroupLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import java.util.Random;
 
 public class test extends JFrame{
 	public static void main(String[] args) throws IOException {
 		Random input = new Random();
 		int array[] = new int[50];
-		for(int i : range(50)) {
+		for(int i = 0; i < 50; i++) {
 			array[i] = input.nextInt(100);
 		}
 		System.out.println(Arrays.toString(array));
